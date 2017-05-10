a0fed1b4c5f89d0c9bd04696478622601aee4777
Luiz Renato <luizrenato2001@yahoo.com.br>
2017-05-10 19:11:16 -0300

    alterações POST

--- src/main/java/br/com/lrsantos/controller/ChamadoController.java
+++ src/main/java/br/com/lrsantos/controller/ChamadoController.java
@@ -6,14 +6,17 @@ import javax.transaction.Transactional;
 
 import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.lrsantos.model.ChamadoService;
@@ -68,8 +71,7 @@ public class ChamadoController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<String> inclui(ChamadoTecnico chamado) {
-		String retorno="";
+	public ResponseEntity<String> inclui(@RequestBody ChamadoTecnico chamado) {
 		ResponseEntity<String> response = null;
 		try {
 			this.validaChamado(chamado);
@@ -83,6 +85,7 @@ public class ChamadoController {
 	}
 
 	private void validaChamado(ChamadoTecnico chamado) {
+		System.out.println("Recebendo chamado: \n"+ chamado);
 		if(chamado==null) {
 			throw new RuntimeException("Chamado invalido");
 		}
